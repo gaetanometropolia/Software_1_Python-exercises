602948070ef73c6092b6a92c370085d3e5eff02b
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-01-28 22:19:24 +0200

    success

---
+++ Module_6_exercises/ex 6.1.py
@@ -0,0 +1,15 @@
+# Write a function that returns a random dice roll between 1 and 6. The function should not have any parameters. Write a main program that rolls the dice until the result is 6. The main program should print out the result of each roll.
+
+import random
+
+def roll_dice():
+    return random.randint(1, 6)
+
+
+def main():
+    roll = 0
+    while roll != 6:
+        roll = roll_dice()
+        print (roll)
+
+main()
