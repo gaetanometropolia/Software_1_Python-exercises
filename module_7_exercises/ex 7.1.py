7d80d91a794ff21f6855e055b053e641c7dcfff5
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-02-02 21:56:37 +0200

    success

---
+++ module_7_exercises/ex 7.1.py
@@ -0,0 +1,16 @@
+# Write a program that asks the user for a number of a month and then prints out the corresponding season (spring, summer, autumn, winter). Save the seasons as strings into a tuple in your program. We can define each season to last three months, December being the first month of winter.
+
+seasons = ("spring", "summer", "autumn", "winter")
+
+month = int(input("Enter a number: "))
+
+if month == 12 or month == 1 or month == 2:
+    print(seasons[3])
+elif month == 3 or month == 4 or month == 5:
+    print(seasons[0])
+elif month == 6 or month == 7 or month == 8:
+    print(season[1])
+elif month == 9 or month == 10 or month == 11:
+    print (seaso[2])
+else:
+    print("no valid number")
